b3ff37954a4b731d12d9f536a360271251e87081
Muhammad <muhammadraza0047@gmail.com>
2018-10-13 00:49:33 -0400

    commiting all files

--- cma/cma/settings.py
+++ cma/cma/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'parents',
     'children',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = '/grades'
